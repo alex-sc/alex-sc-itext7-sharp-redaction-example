ce8bfd18311e0f00bd3b6f98cb017eec36759007
Alex <olexiy.palinkash@gmail.com>
2024-06-07 13:59:19 +0300

    Add sample code with dependencies

---
+++ itext7-redaction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using iText.Kernel.Colors;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.PdfCleanup;
+
+var inFileName = @"../../../../pdfSweep-whitepaper.pdf";
+var outFileName = @"../../../../pdfSweep-whitepaper-redacted.pdf";
+
+PdfDocument pdf = new PdfDocument(new PdfReader(inFileName), new PdfWriter(outFileName));
+
+IList<PdfCleanUpLocation> cleanUpLocations = new Collection<PdfCleanUpLocation>();
+for (var i = 0; i < pdf.GetNumberOfPages(); i++)
+{
+    cleanUpLocations.Add(new PdfCleanUpLocation(i + 1, new Rectangle(200, 200, 200, 200), ColorConstants.BLACK));
+}
+
+PdfCleaner.CleanUp(pdf, cleanUpLocations);
+pdf.Close();
